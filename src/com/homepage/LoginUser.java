ff7a175767ae1d150377aa95e285fcf3572dc4db
charu07goel <charu07goel@gmail.com>
2019-10-10 22:53:34 +0530

    LoginUser

---
+++ src/com/homepage/LoginUser.java
@@ -0,0 +1,56 @@
+package com.homepage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
+
+@WebServlet("/LoginUser")
+public class LoginUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		System.out.println(email);	
+		String pass = request.getParameter("password");
+		System.out.println(pass);
+		PrintWriter out = response.getWriter();
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/complaint_system", "root",
+					"abcdef");
+
+			Statement stmnt = con.createStatement();
+			ResultSet rs = stmnt.executeQuery("SELECT * FROM user_details where email='"+email+"' and password='"+pass+"'");
+			if(rs.next()){
+				out.println("Login Successful");
+//				JOptionPane.showMessageDialog(null, "Login Successfully....");
+			}else{
+				out.println("Incorrect Username or Password");
+//				JOptionPane.showMessageDialog(null, "Incorrect Username or Password");
+			}
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+
+}
