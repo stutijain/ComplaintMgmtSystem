a6818e954fe870cba422b7c2cf50c73b6b3935f1
charu07goel <charu07goel@gmail.com>
2019-10-14 22:11:39 +0530

    register user changes

--- src/com/homepage/RegisterUser.java
+++ src/com/homepage/RegisterUser.java
@@ -22,7 +22,7 @@ public class RegisterUser extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
-	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException{
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
 		User user = new User();	
 		user.setCategory(request.getParameter("category"));
 		user.setName((String) request.getParameter("name"));
